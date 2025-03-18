4aafb28e40b2edb0aa93b02bed20d2ae173a3021
Petr-Barabas <petr.barabas@magicware.cz>
2025-03-18 08:27:18 +0100

    added tasks in main page

--- src/components/folderToEdit/pageToEdit.js
+++ src/components/folderToEdit/pageToEdit.js
@@ -2,11 +2,22 @@ import React from "react";
 
 const PageToEdit = () => (
   <div>
-    <h2>This is main page</h2>
+    <h1>This is main page</h1>
     <p>Here you should add your content</p>
     <a href="https://app.pluralsight.com/library/courses/react-redux-react-router-es6/table-of-contents">
       For better understanding is highly recommended to check this course out
     </a>
+    <div>
+      <h2>Your tasks</h2>
+      <ul>
+        <li>Fetch data</li>
+        <li>Add form to save data</li>
+        <li>
+          Add form to update data (you can use the same as for saving data)
+        </li>
+        <li>Delete data</li>
+      </ul>
+    </div>
   </div>
 );
 
